7b03bec09373015dcd6eb0836c856eaa8b5bf3a2
berliants <berliantrisaputra@gmail.com>
2022-03-06 05:59:50 +0700

    u

--- app.js
+++ app.js
@@ -16,12 +16,11 @@ app.get('/', (request, response) => {
     response.render('home.ejs')
 })
 app.post('/', (request, response) => {
-    const nomor = 123
     response.render('template_pdf.ejs', {data: 'halo'}, (err, html) => {
-        pdf.create(html, options).toFile('./public/output/saya.pdf', (err, result) => {
+        pdf.create(html, options).toFile('./public/output/output.pdf', (err, result) => {
             if(err) console.log(err)
 
-            const datafile = fs.readFileSync('./public/output/saya.pdf')
+            const datafile = fs.readFileSync('./public/output/output.pdf')
             response.header('content-type', 'application/pdf')
             response.send(datafile)
         })
